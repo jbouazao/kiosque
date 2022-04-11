0e89f0e18c7c28a791cbcbdf6e020edd3c86b496
jihad <47062331+jbouazao@users.noreply.github.com>
2022-04-11 23:08:11 +0000

    slider

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,13 +1,17 @@
+import Slider from 'react-slick'
 import classes from './Options.module.css'
-import {AiFillCar, AiFillHeart} from 'react-icons/ai'
-import {FcBusiness} from 'react-icons/fc'
-import {BsNewspaper} from 'react-icons/bs'
-import {HiDesktopComputer} from 'react-icons/hi'
-import {GiKnifeFork, GiPaintBrush} from 'react-icons/gi'
-import {RiComputerFill} from 'react-icons/ri'
-import {FaShapes} from 'react-icons/fa'
-import {SiApplemusic} from 'react-icons/si'
-import {GiMaterialsScience} from 'react-icons/gi'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { AiFillCar, AiFillHeart } from 'react-icons/ai'
+import { FcBusiness } from 'react-icons/fc'
+import { BsNewspaper } from 'react-icons/bs'
+import { HiDesktopComputer } from 'react-icons/hi'
+import { GiKnifeFork, GiPaintBrush } from 'react-icons/gi'
+import { RiComputerFill } from 'react-icons/ri'
+import { FaShapes } from 'react-icons/fa'
+import { SiApplemusic } from 'react-icons/si'
+import { GiMaterialsScience } from 'react-icons/gi'
+import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs'
 
 const optionsData = [
 	{
@@ -64,14 +68,34 @@ const optionsData = [
 	}
 ]
 
+const Arrow = (props) => {
+	return (
+		<div className={classes.prevarrow} onClick = {props.onClick}>
+			{props.direction === 'left' ? <BsArrowLeftCircleFill/> : <BsArrowRightCircleFill/>}
+			{console.log(props)}
+		</div>
+	);
+}
+
+const settings = {
+	speed: 500,
+	infinite: true,
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	prevArrow: <Arrow direction = 'left'/>,
+	nextArrow: <Arrow direction = 'right'/>
+};
+
 const Options = props => {
-	return <ul className = {classes.options}>
-		{/* {optionsData.map(item =>
-			<OptionBtn>{item.text}</OptionBtn>)} */}
-			{optionsData.map(item => <li className = {classes.listitem}>
-				<item.icon style = {{width: '30px', height: '30px', color: 'grey'}}/>
-				{item.text}</li>)}
-	</ul>
+	return (
+		<Slider {...settings} className={classes.options}>
+			{optionsData.map(item =>
+				<div className={classes.listitem}>
+					<item.icon style={{ width: '30px', height: '30px', color: 'grey' }} />
+					{item.text}</div>
+			)}
+		</Slider>
+	)
 }
 
 export default Options;
